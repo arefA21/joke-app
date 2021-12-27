b31e1a9898da557b83ac2247b64f383acf5b7840
arefA21 <87755855+arefA21@users.noreply.github.com>
2021-12-27 08:11:29 -0800

    Update Nav.js

--- myapp/src/components/Nav.js
+++ myapp/src/components/Nav.js
@@ -4,17 +4,16 @@ export default function Nav() {
   return (
     <div>
      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <a class="navbar-brand" href="/">Dad Joke App</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-link active" href="/about">About <span class="sr-only">(current)</span></a>
-      
-    </div>
-  </div>
-</nav>
+      <a class="navbar-brand" href="/">Dad Joke App</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link active" href="/about">About <span class="sr-only">(current)</span></a>  
+      </div>
+       </div>
+      </nav>
     </div>
   );
 }
